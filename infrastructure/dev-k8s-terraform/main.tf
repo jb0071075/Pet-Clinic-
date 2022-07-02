7d463acf2241c0118681bce2b8fa71bbfe5efb3c
jb0071075 <jb0071075@gmail.com>
2022-07-02 13:15:11 +0000

    added dev-k8s-terraform  for kubernetes infrastructure

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -1,22 +1,19 @@
 provider "aws" {
   region  = "us-east-1"
 }
-
 module "iam" {
   source = "./modules/IAM"
 }
-
-resource "aws_security_group" "matt-kube-mutual-sg" {
-  name = "kube-mutual-sec-group-for-matt"
+resource "aws_security_group" "mattt-kube-mutual-sg" {
+  name = "jkube-mutual-sec-group-for-mattt"
 }
-
-resource "aws_security_group" "matt-kube-worker-sg" {
-  name = "kube-worker-sec-group-for-matt"
+resource "aws_security_group" "mattt-kube-worker-sg" {
+  name = "jkube-worker-sec-group-for-mattt"
   ingress {
     protocol = "tcp"
     from_port = 10250
     to_port = 10250
-    security_groups = [aws_security_group.matt-kube-mutual-sg.id]
+    security_groups = [aws_security_group.mattt-kube-mutual-sg.id]
   }
   ingress {
     protocol = "tcp"
@@ -24,21 +21,18 @@ resource "aws_security_group" "matt-kube-worker-sg" {
     to_port = 32767
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     protocol = "tcp"
     from_port = 22
     to_port = 22
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     protocol = "udp"
     from_port = 8472
     to_port = 8472
-    security_groups = [aws_security_group.matt-kube-mutual-sg.id]
+    security_groups = [aws_security_group.mattt-kube-mutual-sg.id]
   }
-  
   egress{
     protocol = "-1"
     from_port = 0
@@ -47,13 +41,11 @@ resource "aws_security_group" "matt-kube-worker-sg" {
   }
   tags = {
     Name = "kube-worker-secgroup"
-    "kubernetes.io/cluster/mattsCluster" = "owned"
+    "kubernetes.io/cluster/matttsCluster" = "owned"
   }
 }
-
-resource "aws_security_group" "matt-kube-master-sg" {
-  name = "kube-master-sec-group-for-matt"
-
+resource "aws_security_group" "mattt-kube-master-sg" {
+  name = "jkube-master-sec-group-for-mattt"
   ingress {
     protocol = "tcp"
     from_port = 22
@@ -71,7 +63,7 @@ resource "aws_security_group" "matt-kube-master-sg" {
     from_port = 6443
     to_port = 6443
     cidr_blocks = ["0.0.0.0/0"]
-    #security_groups = [aws_security_group.matt-kube-mutual-sg.id]
+    #security_groups = [aws_security_group.mattt-kube-mutual-sg.id]
   }
   ingress {
     protocol = "tcp"
@@ -83,31 +75,31 @@ resource "aws_security_group" "matt-kube-master-sg" {
     protocol = "tcp"
     from_port = 2380
     to_port = 2380
-    security_groups = [aws_security_group.matt-kube-mutual-sg.id]
+    security_groups = [aws_security_group.mattt-kube-mutual-sg.id]
   }
   ingress {
     protocol = "tcp"
     from_port = 2379
     to_port = 2379
-    security_groups = [aws_security_group.matt-kube-mutual-sg.id]
+    security_groups = [aws_security_group.mattt-kube-mutual-sg.id]
   }
   ingress {
     protocol = "tcp"
     from_port = 10250
     to_port = 10250
-    security_groups = [aws_security_group.matt-kube-mutual-sg.id]
+    security_groups = [aws_security_group.mattt-kube-mutual-sg.id]
   }
   ingress {
     protocol = "tcp"
     from_port = 10251
     to_port = 10251
-    security_groups = [aws_security_group.matt-kube-mutual-sg.id]
+    security_groups = [aws_security_group.mattt-kube-mutual-sg.id]
   }
   ingress {
     protocol = "tcp"
     from_port = 10252
     to_port = 10252
-    security_groups = [aws_security_group.matt-kube-mutual-sg.id]
+    security_groups = [aws_security_group.mattt-kube-mutual-sg.id]
   }
   ingress {
     protocol = "tcp"
@@ -119,7 +111,7 @@ resource "aws_security_group" "matt-kube-master-sg" {
     protocol = "udp"
     from_port = 8472
     to_port = 8472
-    security_groups = [aws_security_group.matt-kube-mutual-sg.id]
+    security_groups = [aws_security_group.mattt-kube-mutual-sg.id]
   }
   egress {
     protocol = "-1"
@@ -128,76 +120,70 @@ resource "aws_security_group" "matt-kube-master-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
-    Name = "kube-master-secgroup"
+    Name = "jkube-master-secgroup"
   }
 }
-
 resource "aws_instance" "kube-master" {
     ami = "ami-013f17f36f8b1fefb"
     instance_type = "t2.medium"
     iam_instance_profile = module.iam.master_profile_name
-    vpc_security_group_ids = [aws_security_group.matt-kube-master-sg.id, aws_security_group.matt-kube-mutual-sg.id]
-    key_name = "mattkey"
+    vpc_security_group_ids = [aws_security_group.mattt-kube-master-sg.id, aws_security_group.mattt-kube-mutual-sg.id]
+    key_name = "ssh-key"
     subnet_id = "subnet-0d77bc21e25fb29c4"  # select own subnet_id of us-east-1a
     availability_zone = "us-east-1a"
     tags = {
-        Name = "kube-master"
-        "kubernetes.io/cluster/mattsCluster" = "owned"
+        Name = "jkube-master"
+        "kubernetes.io/cluster/matttsCluster" = "owned"
         Project = "tera-kube-ans"
         Role = "master"
         Id = "1"
         environment = "dev"
     }
 }
-
 resource "aws_instance" "worker-1" {
     ami = "ami-013f17f36f8b1fefb"
     instance_type = "t2.medium"
         iam_instance_profile = module.iam.worker_profile_name
-    vpc_security_group_ids = [aws_security_group.matt-kube-worker-sg.id, aws_security_group.matt-kube-mutual-sg.id]
-    key_name = "mattkey"
+    vpc_security_group_ids = [aws_security_group.mattt-kube-worker-sg.id, aws_security_group.mattt-kube-mutual-sg.id]
+    key_name = "ssh-key"
     subnet_id = "subnet-0d77bc21e25fb29c4"  # select own subnet_id of us-east-1a
     availability_zone = "us-east-1a"
     tags = {
-        Name = "worker-1"
-        "kubernetes.io/cluster/mattsCluster" = "owned"
+        Name = "jworker-1"
+        "kubernetes.io/cluster/matttsCluster" = "owned"
         Project = "tera-kube-ans"
         Role = "worker"
         Id = "1"
         environment = "dev"
     }
 }
-
 resource "aws_instance" "worker-2" {
     ami = "ami-013f17f36f8b1fefb"
     instance_type = "t2.medium"
     iam_instance_profile = module.iam.worker_profile_name
-    vpc_security_group_ids = [aws_security_group.matt-kube-worker-sg.id, aws_security_group.matt-kube-mutual-sg.id]
-    key_name = "mattkey"
+    vpc_security_group_ids = [aws_security_group.mattt-kube-worker-sg.id, aws_security_group.mattt-kube-mutual-sg.id]
+    key_name = "ssh-key"
     subnet_id = "subnet-0d77bc21e25fb29c4"  # select own subnet_id of us-east-1a
     availability_zone = "us-east-1a"
     tags = {
-        Name = "worker-2"
-        "kubernetes.io/cluster/mattsCluster" = "owned"
+        Name = "jworker-2"
+        "kubernetes.io/cluster/matttsCluster" = "owned"
         Project = "tera-kube-ans"
         Role = "worker"
         Id = "2"
         environment = "dev"
     }
 }
-
 output kube-master-ip {
   value       = aws_instance.kube-master.public_ip
   sensitive   = false
   description = "public ip of the kube-master"
 }
-
 output worker-1-ip {
   value       = aws_instance.worker-1.public_ip
   sensitive   = false
   description = "public ip of the worker-1"
 }
-
 output worker-2-ip {
   value       = aws_instance.worker-2.public_ip
   sensitive   = false
--- infrastructure/dev-k8s-terraform/modules/IAM/roles.tf
+++ infrastructure/dev-k8s-terraform/modules/IAM/roles.tf
@@ -1,16 +1,13 @@
 resource "aws_iam_policy" "policy_for_master_role" {
-  name        = "policy_for_master_role"
+  name        = "jpolicy_for_master_role"
   policy      = file("./modules/IAM/policy_for_master.json")
 }
-
 resource "aws_iam_policy" "policy_for_worker_role" {
-  name        = "policy_for_worker_role"
+  name        = "jpolicy_for_worker_role"
   policy      = file("./modules/IAM/policy_for_worker.json")
 }
-
 resource "aws_iam_role" "role_for_master" {
-  name = "role_master_k8s"
-
+  name = "jrole_master_k8s"
   # Terraform "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
   assume_role_policy = jsonencode({
@@ -26,15 +23,12 @@ resource "aws_iam_role" "role_for_master" {
       },
     ]
   })
-
   tags = {
-    Name = "role_for_master"
+    Name = "jrole_for_master"
   }
 }
-
 resource "aws_iam_role" "role_for_worker" {
-  name = "role_worker_k8s"
-
+  name = "jrole_worker_k8s"
   # Terraform "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
   assume_role_policy = jsonencode({
@@ -50,38 +44,31 @@ resource "aws_iam_role" "role_for_worker" {
       },
     ]
   })
-
   tags = {
-    Name = "role_for_worker"
+    Name = "jrole_for_worker"
   }
 }
-
 resource "aws_iam_policy_attachment" "attach_for_master" {
-  name       = "attachment_for_master"
+  name       = "jattachment_for_master"
   roles      = [aws_iam_role.role_for_master.name]
   policy_arn = aws_iam_policy.policy_for_master_role.arn
 }
-
 resource "aws_iam_policy_attachment" "attach_for_worker" {
-  name       = "attachment_for_worker"
+  name       = "jattachment_for_worker"
   roles      = [aws_iam_role.role_for_worker.name]
   policy_arn = aws_iam_policy.policy_for_worker_role.arn
 }
-
 resource "aws_iam_instance_profile" "profile_for_master" {
-  name  = "profile_for_master"
+  name  = "jprofile_for_master"
   role = aws_iam_role.role_for_master.name
 }
-
 resource "aws_iam_instance_profile" "profile_for_worker" {
-  name  = "profile_for_worker"
+  name  = "jprofile_for_worker"
   role = aws_iam_role.role_for_worker.name
 }
-
 output master_profile_name {
   value       = aws_iam_instance_profile.profile_for_master.name
 }
-
 output worker_profile_name {
   value       = aws_iam_instance_profile.profile_for_worker.name
 }
